d5a16439e9d69dae7be5a3524ad753884c0add3b
geocjohns4312 <geocjohns@gmail.com>
2025-03-11 10:49:10 +0530

    changing bucket name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket-4312"
+    bucket         = "my-terraform-state-bucket-geo4312"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
