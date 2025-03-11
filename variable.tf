221e54910d3ae19879d820cc7df0438ca5736f88
geocjohns4312 <geocjohns@gmail.com>
2025-03-11 14:03:04 +0530

    created modules

--- variable.tf
+++ variable.tf
@@ -1,3 +1,25 @@
 variable "region" {
   description = "AWS Region"
 }
+
+
+variable "db_username" {
+  description = "psql username"
+}
+
+variable "db_password" {
+  description = "psql password"
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs in different AZs"
+  type        = list(string)
+}
+
+variable "subnet_id" {
+  description = "Sonar Subnet ID"
+}
